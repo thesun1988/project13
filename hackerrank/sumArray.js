f92e5e849bb2f06a085e471c491528156cd51fe1
thesun1988 <thesun_1988@yahoo.com>
2023-11-06 22:18:21 -0800

    Update sumArray.js
    update solution

--- hackerrank/sumArray.js
+++ hackerrank/sumArray.js
@@ -1,13 +1,54 @@
-Given an array of integers, find the sum of its elements.
-For example, if the array , , so return .
-Function Description
-Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
-simpleArraySum has the following parameter(s):
-ar: an array of integers
-Input Format
-The first line contains an integer, , denoting the size of the array. 
-The second line contains  space-separated integers representing the array's elements.
-Constraints
+// Given an array of integers, find the sum of its elements.
+// For example, if the array , , so return .
+// Function Description
+// Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+// simpleArraySum has the following parameter(s):
+// ar: an array of integers
+// Input Format
+// The first line contains an integer, , denoting the size of the array. 
+// The second line contains  space-separated integers representing the array's elements.
+// Constraints
 
-Output Format
-Print the sum of the array's elements as a single integer.
+// Output Format
+// Print the sum of the array's elements as a single integer.
+
+//Solution:
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
